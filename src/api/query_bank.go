0c1d2df433a721a7fc054652593adab649cefe54
Sungju bong <bongster88@gmail.com>
2022-03-16 00:09:04 +0800

    add cosmos-sdk module in project

---
+++ src/api/query_bank.go
@@ -0,0 +1,9 @@
+package api
+
+import (
+	"net/http"
+)
+
+func (server *Server) QueryBankTotalHandler(w http.ResponseWriter, r *http.Request) {
+
+}
--- src/api/server.go
+++ src/api/server.go
@@ -1,22 +1,30 @@
 package api
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/cosmos/cosmos-sdk/x/bank"
 )
 
 type Server struct {
 	router *http.ServeMux
 }
 
+// NewServer return server object
 func NewServer() *Server {
 	server := &Server{}
 	router := http.NewServeMux()
 	router.HandleFunc("/healthcheck", server.HealthCheckHandler)
+	bankModule := bank.AppModuleBasic{}
+	fmt.Println(bankModule)
+	router.HandleFunc("/query/bank/total", server.QueryBankTotalHandler)
 	server.router = router
 	return server
 }
 
+// Start running HTTP server
 func (server *Server) Start(address string) error {
 	log.Printf("Server is running %s\n", address)
 	return http.ListenAndServe(address, server.router)
